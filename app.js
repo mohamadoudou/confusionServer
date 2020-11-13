7d079b612d522571fb70056814b19234fc776da8
mohamadou moctar <moctarm99@gmail.com>
2020-11-13 10:24:17 +0600

    express session part1

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session =require('express-session');
+var FileStore= require('session-file-store')(session);
 var logger = require('morgan');
 const mongooose = require('mongoose');
 
@@ -28,12 +30,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized:false,
+  resave:false,
+  store:new FileStore()
+}))
 
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
-    console.log(req.headers);
+  if (!req.session.user) {
+    console.log(req.session);
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -47,7 +56,7 @@ function auth(req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user === 'admin' && pass === 'password') {
-      res.cookie('user','admin',{signed:true});
+      req.session.user='admin';
       next();
     }
     else {
@@ -58,7 +67,7 @@ function auth(req, res, next) {
     }
   }
   else{
-    if(req.signedCookies.user==='admin'){
+    if(req.session.user==='admin'){
       next();
     }
     else{
