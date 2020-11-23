8c9a4a128873282fd5bbf601c0fe92fa0dbdd06a
mohamadou moctar <moctarm99@gmail.com>
2020-11-23 15:11:32 +0600

    passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-module.exports={
-    'secretKey':'12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '824257338358832',
+        clientSecret: 'bda5d30d1afa64be88d82c667764904b'
+    }
 }
