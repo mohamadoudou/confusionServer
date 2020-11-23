e6cd3fae20f71b0b9229f227d3d6e20e6ba5edb5
mohamadou moctar <moctarm99@gmail.com>
2020-11-23 10:45:02 +0600

    file upload

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 var password = require('passport');
 var authenticate=require('./authenticate');
 const { use } = require('./routes/index');
@@ -60,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
